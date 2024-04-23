9ed5e28c8b64d548d3c257cfd9509c13c686e6f4
MultiPlanJSEval <39303958+MultiPlanJSEval@users.noreply.github.com>
2024-04-23 17:03:05 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hey! We are just testing Cloud Front and how-to protect private static resoruces in s3 buckets.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
